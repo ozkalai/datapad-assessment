25b809f2329579e8a4d0305e773ac018eff5f9de
ozkalai <melihozkalayy@gmail.com>
2022-11-15 22:26:37 +0300

    feat: add dnd feature

--- webclient/pages/[workspaceid]/dashboards/[dashboardid].page.tsx
+++ webclient/pages/[workspaceid]/dashboards/[dashboardid].page.tsx
@@ -1,5 +1,8 @@
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { DndContext, DragOverlay, useDroppable } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 import Layout from "@webclient/components/Layout/Layout";
 import Button from "@webclient/components/UI/Button/Button";
 import Title from "@webclient/components/UI/Title/Title";
@@ -9,11 +12,30 @@ import { MetricChart } from "@webclient/components/Dashboards/MetricChart";
 
 function DashboardInner(props) {
   const { workspaceid, dashboardid } = props;
+  const [activeId, setActiveId] = React.useState(null);
+  const [initialIndex, setInitialIndex] = React.useState(null);
+
+  const { setNodeRef } = useDroppable({
+    id: "droppable",
+  });
 
   const { isError, error, isSuccess, status, data } = useDashboardFetch(
     workspaceid as string,
-    dashboardid as string,
+    dashboardid as string
   );
+
+  const [items, setItems] = React.useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setItems(
+        data?.metrics.map((el, index) => ({
+          ...el,
+          id: `metric-${index}`,
+        }))
+      );
+    }
+  }, [data]);
 
   if (isError) {
     return <>error: {JSON.stringify(error)}</>;
@@ -23,20 +45,71 @@ function DashboardInner(props) {
     return <>status: {status}...</>;
   }
 
-  return (
-    <>
-      <Title
-        icon={data.icon}
-        title={data.title}
-        subtitle={data.description}
-      />
+  function handleDragStart(event) {
+    setActiveId(event.active.id);
+    setInitialIndex(items.findIndex((el) => el.id === event.active.id));
+  }
 
-      <div className="grid grid-flow-row-dense grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
-        {Object.entries(data.metrics).map(([metricId, metric]) => {
-          return <MetricChart key={metricId} metric={metric} />;
-        })}
-      </div>
-    </>
+  function handleDragEnd(event) {
+    setActiveId(null);
+  }
+
+  function handleDragOver(event) {
+    const { over } = event;
+
+    if (over?.id !== activeId) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === activeId);
+        const newIndex = items.findIndex((item) => item.id === over?.id);
+
+        const newItems = [...items];
+        const [removed] = newItems.splice(oldIndex, 1);
+        newItems.splice(newIndex, 0, removed);
+
+        return newItems;
+      });
+    }
+  }
+
+  return (
+    <DndContext
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
+    >
+      <SortableContext items={items}>
+        <Title
+          icon={data.icon}
+          title={data.title}
+          subtitle={data.description}
+        />
+        <div
+          ref={setNodeRef}
+          className="grid grid-flow-row-dense grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6"
+        >
+          {items.map((metric, index) => {
+            return (
+              <React.Fragment key={metric.id}>
+                <MetricChart
+                  isInitialIndex={initialIndex === index}
+                  activeId={activeId}
+                  metric={metric}
+                />
+              </React.Fragment>
+            );
+          })}
+          <DragOverlay>
+            {activeId ? (
+              <MetricChart
+                isOverlay
+                key={activeId}
+                metric={items.find((el) => el.id === activeId)}
+              />
+            ) : null}
+          </DragOverlay>
+        </div>
+      </SortableContext>
+    </DndContext>
   );
 }
 
